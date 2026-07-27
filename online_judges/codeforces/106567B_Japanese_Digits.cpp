82cec3b6636e821eab466c0df17e2c82a7f9ce6f
Muhammad Abdullah <152422522+Abdullah-78gh@users.noreply.github.com>
2026-07-27 23:39:40 +0600

    Sync Codeforces: 106567B - Japanese Digits [C++23 (GCC 14-64, msys2)]

---
+++ online_judges/codeforces/106567B_Japanese_Digits.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ 
+void solve() {
+    cout << "nanani\n";
+}
+ 
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    // int t; cin >> t; while(t--)
+    solve();
+}
